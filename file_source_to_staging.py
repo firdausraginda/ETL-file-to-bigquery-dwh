7e39052ea174ce182ae59c220dd7806bf5efe358
raginda firdaus <ragindafirdaus01@gmail.com>
2021-10-16 19:23:33 +0700

    set path to json & csv files and loop over the file names

---
+++ file_source_to_staging.py
@@ -0,0 +1,59 @@
+from google.cloud import bigquery
+import pandas as pd
+from bigquery.setup import create_bq_client
+import pandas_gbq
+from pathlib import Path
+import os
+
+
+def read_csv_file(path_to_csv):
+    """read csv file and do minor data type conversion, return it as dataframe"""
+
+    df = pd.read_csv(path_to_csv, na_values="T", dtype={"precipitation": float}, parse_dates=["date"])
+    
+    return df
+
+
+def read_json_file(path_to_json):
+    """read json file and return it as dataframe"""
+
+    df = pd.read_json(path_to_json, lines=True)
+
+    return df
+
+
+def write_to_staging(df):
+    """write data from csv/json file to staging dataset with respective table name"""
+
+    client = create_bq_client()
+    dataset_name = "project_1_staging"
+    table_name = "precipitation"
+
+    pandas_gbq.to_gbq(
+        df, f"{dataset_name}.{table_name}", project_id=client.project, if_exists="replace"
+    )
+
+    return None
+
+
+if __name__ == "__main__":
+
+    current_path = Path(__file__).absolute()
+
+    # get path to csv files
+    path_to_csv = current_path.parent.joinpath("src/weather")
+    list_csv = os.listdir(path_to_csv)
+
+    # get path to json files
+    path_to_json = current_path.parent.joinpath("src/yelp_reviews")
+    list_json = os.listdir(path_to_json)
+    
+    for csv in list_csv:
+        print(path_to_csv.joinpath(csv))
+
+
+    # print(precipitation_df.head())
+    # print(temperature_df.head())
+    # print(temperature_df.info())
+    # print(business_df.head())
+    # print(business_df.info())
--- load_files_to_bq.py
+++
@@ -1,6 +0,0 @@
-from google.cloud import bigquery
-import pandas as pd
-
-table_id = 'my_dataset.new_table'
-
-df.to_gbq(table_id)
