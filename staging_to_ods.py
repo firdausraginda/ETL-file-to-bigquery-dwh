6734c4bfcc3fffe1e7e55f7c2abbf9eae497440d
raginda firdaus <ragindafirdaus01@gmail.com>
2021-10-17 19:37:25 +0700

    create sql transform for temperature_degreef

--- staging_to_ods.py
+++ staging_to_ods.py
@@ -36,22 +36,21 @@ def transform_precipitation():
 
 def transform_temperature():
 
-    # get sql query to read table
+    sql = """
+        SELECT
+            PARSE_DATE('%Y%m%d', CAST(date AS STRING)) AS date,
+            SAFE_CAST(min AS FLOAT64) AS min,
+            SAFE_CAST(max AS FLOAT64) AS max,
+            SAFE_CAST(normal_min AS FLOAT64) AS normal_min,
+            SAFE_CAST(normal_max AS FLOAT64) AS normal_max
+        FROM
+        `dummy-329203.project_1_staging.temperature_degreef`
+        """
+
     client = create_bq_client()
-    table_name = 'temperature_degreef'
-    sql = sql_read_query(table_name)
 
     # read table as dataframe
     df = pandas_gbq.read_gbq(sql, project_id=client.project)
-
-    # convert date from int to datetime
-    df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
-
-    # convert min, max, normal_min, & normal_max to float with error handling to NaN
-    df['min'] = pd.to_numeric(df['min'], errors='coerce')
-    df['max'] = pd.to_numeric(df['max'], errors='coerce')
-    df['normal_min'] = pd.to_numeric(df['normal_min'], errors='coerce')
-    df['normal_max'] = pd.to_numeric(df['normal_max'], errors='coerce')
 
     return df
 
@@ -142,7 +141,7 @@ def transform_yelp_business():
     client = create_bq_client()
 
     # read table as dataframe
-    df = pandas_gbq.read_gbq(sql, project_id=client.project)    
+    df = pandas_gbq.read_gbq(sql, project_id=client.project)
 
     return df
 
@@ -150,8 +149,8 @@ def transform_yelp_business():
 if __name__ == "__main__":
     
     precipitation_df = transform_precipitation()
-    # # temperature_df = transform_temperature()
-    # yelp_business_df = transform_yelp_business()
+    temperature_df = transform_temperature()
+    yelp_business_df = transform_yelp_business()
 
-    print(precipitation_df)
-    print(precipitation_df.info())
+    print(temperature_df)
+    print(temperature_df.info())
