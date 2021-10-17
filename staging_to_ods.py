996e4b261c7d71cfbf50a791f1f71649dc4ba4fb
raginda firdaus <ragindafirdaus01@gmail.com>
2021-10-17 16:26:51 +0700

    create transform function for temperature_degreef data

--- staging_to_ods.py
+++ staging_to_ods.py
@@ -30,15 +30,38 @@ def transform_precipitation():
     # convert date from int to datetime
     df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
 
-    # convert precipitation from object to float with error handling to NaN
+    # convert precipitation & precipitation_normal to float with error handling to NaN
     df['precipitation'] = pd.to_numeric(df['precipitation'], errors='coerce')
+    df['precipitation_normal'] = pd.to_numeric(df['precipitation_normal'], errors='coerce')
 
-    print(df)
-    print(df.info())
+    return df
+
+
+def transform_temperature():
+
+    # get sql query to read table
+    table_name = 'temperature_degreef'
+    sql = sql_read_query(table_name)
+
+    # read table as dataframe
+    df = pandas_gbq.read_gbq(sql, project_id=client.project)
+
+    # convert date from int to datetime
+    df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
+
+    # convert min, max, normal_min, & normal_max to float with error handling to NaN
+    df['min'] = pd.to_numeric(df['min'], errors='coerce')
+    df['max'] = pd.to_numeric(df['max'], errors='coerce')
+    df['normal_min'] = pd.to_numeric(df['normal_min'], errors='coerce')
+    df['normal_max'] = pd.to_numeric(df['normal_max'], errors='coerce')
 
     return df
 
 
 if __name__ == "__main__":
     
-    transform_precipitation()
+    precipitation_df = transform_precipitation()
+    temperature_df = transform_temperature()
+
+    print(temperature_df)
+    print(temperature_df.info())
