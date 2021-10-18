42a3f1e101db59b97ab4fa9db397fcdd8887511e
raginda firdaus <ragindafirdaus01@gmail.com>
2021-10-18 09:30:33 +0700

    distinct row yelp_tip.sql

--- sql_transforms/yelp_tip.sql
+++ sql_transforms/yelp_tip.sql
@@ -1,9 +1,22 @@
-SELECT
-    concat(user_id, '-', business_id) as tip_id,
+WITH
+    coonstruct_tip_id AS (
+    SELECT
+        concat(user_id, '-', business_id) as tip_id,
+        user_id,
+        business_id,
+        text,
+        PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S', date) AS date,
+        SAFE_CAST(compliment_count AS INT64) AS compliment_count
+    FROM
+        `dummy-329203.project_1_staging.yelp_tip`
+    )
+
+SELECT 
+    distinct(tip_id),
     user_id,
     business_id,
     text,
-    PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S', date) AS date,
-    SAFE_CAST(compliment_count AS INT64) AS compliment_count
-FROM
-    `dummy-329203.project_1_staging.yelp_tip`
+    date,
+    compliment_count
+FROM 
+    coonstruct_tip_id
